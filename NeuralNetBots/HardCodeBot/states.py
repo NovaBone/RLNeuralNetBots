45b9ff388091fdd1be3c8fb5f28ce619a03d143e
NovaBone <butterdogarmybone@gmail.com>
2018-12-02 13:33:59 -0500

    Rename Files

---
+++ NeuralNetBots/HardCodeBot/states.py
@@ -0,0 +1,46 @@
+class StatesHandler:
+    def __init__(self):
+        pass
+
+    def choosestate(self, ball, me, others):
+        # Example list of state functions
+        self.statelist = {
+            'dribble': self.atba(ball,me,others),
+            'flick': "call()",
+            'pass': "call()",
+            '0': "call()",
+            '1': "call()",
+            '2': "call()",
+            '3': "call()",
+            '4': "call()",
+            '5': "call()",
+            '6': "call()",
+            '7': "call()",
+            '8': "call()",
+            '9': "call()"
+        }
+
+    def execute(self, state):
+        if x :'''state incomplete'''
+            self.stateList[state]
+        else:
+            return {
+                'Finished': True,
+                'throttle': 0,
+                'steer': 0,
+                'pitch': 0,
+                'yaw': 0,
+                'roll': 0,
+                'jump': 0,
+                'boost': 0,
+                'handbrake': 0
+            }
+
+
+class States:
+
+    def __init__(self):
+        pass
+
+    def atba(self,ball,me,others):
+        pass
