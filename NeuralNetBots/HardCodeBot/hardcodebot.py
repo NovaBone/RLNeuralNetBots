aa33e31a41a16bf3a5c1f7b11d5ed351d0d0567c
NovaBone <butterdogarmybone@gmail.com>
2018-12-17 11:24:14 -0500

    Rename Files

--- NeuralNetBots/HardCodeBot/hardcodebot.py
+++ NeuralNetBots/HardCodeBot/hardcodebot.py
@@ -1,43 +1,234 @@
 import math
-import random
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from numpy import *
+from NeuralNetBots.HardCodeBot.States import StatesHandler, CurrentState
 
 
 class HardCodeBot(BaseAgent):
-    
+
     def initialize_agent(self):
         self.controller_state = SimpleControllerState()
+        self.last_touch = {'name': "", 'time': 0.0, 'timesince': 0.0}
+        self.myusername = ""
 
-    def get_output(self, game: GameTickPacket) -> SimpleControllerState:
-        self.preprocess(game)
+    def get_output(self, game: GameTickPacket, rigidbody: BaseAgent.get_rigid_body_tick()) -> SimpleControllerState:
+        state = "ground state"
+        preprocess = self.preprocess(game, rigidbody)
+        for i in StatesHandler.state:
+            if StatesHandler.state[i]:
+                state = StatesHandler.state[i]
+        output = StatesHandler.execute(HardCodeBot.States.StateHandler, state, preprocess['ball'],
+                                       preprocess['me'], preprocess['others'], preprocess['info'])
+        self.controllerstate.throttle = output['throttle']
+        self.controllerstate.steer = output['steer']
+        self.controllerstate.pitch = output['pitch']
+        self.controllerstate.yaw = output['yaw']
+        self.controllerstate.roll = output['roll']
+        self.controllerstate.jump = output['jump']
+        self.controllerstate.boost = output['boost']
+        self.controllerstate.handbrake = output['handbrake']
         return self.controller_state
 
-    
-    def preprocess(self,game):
+    def preprocess(self, game, body):
         car = game.game_cars[self.index]
-        mylocationdata = {"x": car.physics.location.x, "y": car.physics.location.y, "z": car.physics.location.z}
-        myvelocitydata = {"x": car.physics.velocity.x, "y": car.physics.velocity.y, "z": car.physics.velocity.z}
-        myrotationdata = {"x": car.physics.rotation.pitch, "y": car.physics.rotation.yaw, "z": car.physics.rotation.roll}
-        myrvelocitydata = {"x": car.physics.angular_velocity.x, "y": car.physics.angular_velocity.y, "z": car.physics.angular_velocity.z}
+        mycar = body.players[self.index]
+        wheelcontact = False
+        if car.has_wheel_contact:
+            wheelcontact = True
+        doublejumped = True
+        if not car.double_jumped:
+            doublejumped = False
+        mylocationdata = {"x": mycar.state.location.x, "y": mycar.state.location.y, "z": mycar.state.location.z}
+        myvelocitydata = {"x": mycar.state.velocity.x, "y": mycar.state.velocity.y, "z": mycar.state.velocity.z}
+        myrotation = UtilFunctions.quaterniontoeuler(mycar.state.rotation)
+        myrotationdata = {"x": myrotation['pitch'], "y": myrotation['yaw'],
+                          "z": myrotation['roll']}
+        myrvelocitydata = {"x": mycar.state.angular_velocity.x, "y": mycar.state.angular_velocity.y,
+                           "z": mycar.state.angular_velocity.z}
         myboost = car.boost
+        mystats = {'locationdata': mylocationdata, 'velocitydata': myvelocitydata, 'rotationdata': myrotationdata,
+                   'rvelocitydata': myrvelocitydata, 'boost': myboost, 'doublejumped': doublejumped,
+                   'wheelcontact': wheelcontact}
 
-        ball = game.game_ball.physics
+        ball = body.ball
         balllocationdata = {"x": ball.location.x, "y": ball.location.y, "z": ball.location.z}
         ballvelocitydata = {"x": ball.velocity.x, "y": ball.velocity.y, "z": ball.velocity.z}
-        ballrotationdata = {"x": ball.rotation.pitch, "y": ball.rotation.yaw, "z": ball.rotation.roll}
+        rotation = UtilFunctions.quaterniontoeuler(ball.rotation)
+        ballrotationdata = {"x": rotation['pitch'], "y": rotation['yaw'], "z": rotation['roll']}
         ballrvelocitydata = {"x": ball.angular_velocity.x, "y": ball.angular_velocity.y, "z": ball.angular_velocity.z}
+        ballstats = {'location': balllocationdata, 'velocity': ballvelocitydata,
+                     'rotation': ballrotationdata, 'rvelocity': ballrvelocitydata}
 
-        other = []
-        nodes = []
+        otherstats = []
         for i in range(8):
             if i != self.index:
                 car = game.game_cars[i]
-                other.append({"team": car.team,
-                    "locationdata": {"x": car.physics.location.x, "y": car.physics.location.y, "z": car.physics.location.z},
-                    "velocitydata": {"x": car.physics.velocity.x, "y": car.physics.velocity.y, "z": car.physics.velocity.z},
-                    "rotationdata": {"x": car.physics.rotation.pitch, "y": car.physics.rotation.yaw, "z": car.physics.rotation.roll},
-                    "rvelocitydata": {"x": car.physics.angular_velocity.x, "y": car.physics.angular_velocity.y, "z": car.physics.angular_velocity.z},
-                    "boost":0})
+                rigid = body.players[i]
+                wheelcontact = False
+                if car.has_wheel_contact:
+                    wheelcontact = True
+                doublejumped = True
+                if not car.double_jumped:
+                    doublejumped = False
+                teammate = False
+                if car.team == game.game_cars[self.index].team:
+                    teammate = True
+                rotation = UtilFunctions.quaterniontoeuler(rigid.state.rotation)
+                otherstats.append({"teammate": teammate,
+                                   "location": {"x": rigid.state.location.x,
+                                                    "y": rigid.state.location.y,
+                                                    "z": rigid.state.location.z},
+                                   "velocity": {"x": rigid.state.velocity.x,
+                                                    "y": rigid.state.velocity.y,
+                                                    "z": rigid.state.velocity.z},
+                                   "rotation": {"x": rotation['pitch'],
+                                                    "y": rotation['yaw'],
+                                                    "z": rotation['roll']},
+                                   "rvelocity": {"x": rigid.state.angular_velocity.x,
+                                                     "y": rigid.state.angular_velocity.y,
+                                                     "z": rigid.state.angular_velocity.z},
+                                   "boost": 0,
+                                   "doublejumped": doublejumped,
+                                   "wheelcontact": wheelcontact
+                                   })
+        for i in range(len(game.game_cars)):
+            if i == self.index:
+                self.myusername = game.game_cars[i]['name']
+        info = game.game_ball.latest_touch
+        touchinfo = {
+            'player': info.player_name,
+            'time': info.time_seconds
+        }
+        if touchinfo['player'] != self.last_touch['name']:
+            self.last_touch['name'] = touchinfo['player']
+            self.last_touch['time'] = touchinfo['time']
+            self.last_touch['timesince'] = 0.0
+        else:
+            self.last_touch['timesince'] = game.game_info.seconds_elapsed - self.last_touch['time']
+        return {
+            'me': mystats,
+            'ball': ballstats,
+            'others': otherstats,
+            'touchinfo': touchinfo
+        }
+
+
+class UtilFunctions:
+    def distance(self, coords1, coords2):
+        distance = self.magnitude({'x': abs(coords1['x']-coords2['x']),
+                                   'y': abs(coords1['y']-coords2['y']),
+                                   'z': abs(coords1['z']-coords2['z'])})
+        return distance
+
+    def towardsball(self, balllocation, carlocation, carrotation, carvelocity):
+        bl = balllocation
+        cl = carlocation
+        r = carrotation
+        v = carvelocity
+        balltocarvector = {'x': cl['x'] - bl['x'], 'y': cl['y'] - bl['y'], 'z': cl['z'] - bl['z']}
+        if self.hemispherecheck(balltocarvector, r) or self.hemispherecheck(balltocarvector, v):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def magnitude(vector):
+        x = math.sqrt(pow(vector['x'], 2) + pow(vector['y'], 2) + pow(vector['z'], 2))
+        return x
+
+    @staticmethod
+    def dotproduct(vector1, vector2):
+        x = vector1['x'] * vector2['x'] + vector1['y'] * vector2['y'] + vector1['z'] * vector2['z']
+        return x
+
+    def hemispherecheck(self, planevector, checkvector):
+        x = math.acos(
+            self.dotproduct(planevector, checkvector) / (self.magnitude(planevector) * self.magnitude(checkvector)))
+        if x <= math.pi / 2:
+            return True
+        else:
+            return False
+
+    def mintimetoball(self, me, others):
+        '''
+        for i in range(len(others)):
+            if self.touchingground(i, game):
+                # on the ground
+                # Velocity +/- 500 uu/s for flip
+                pass
+            elif self.hasdoublejump(i, game):
+                # has a double jump
+                pass
+            else:
+                # in midair, no flip
+                pass
+        '''
+        pass
+
+    def bounce(self):
+        '''
+        Ball Bounce Prediction Code:
+        ball_prediction = self.get_ball_prediction_struct()
+
+        if ball_prediction is not None:
+            for i in range(0, ball_prediction.num_slices):
+                prediction_slice = ball_prediction.slices[i]
+                location = prediction_slice.physics.location
+                self.logger.info("At time {}, the ball will be at ({}, {}, {})"
+                                 .format(prediction_slice.game_seconds, location.x, location.y, location.z))
+        '''
+        pass
+
+    def hitground(self, ball):
+        pass
+
+    @staticmethod
+    def touchingground(i, game):
+        if game.game_cars[i].has_wheel_contact and game.game_cars[i].physics.location.z <= 80:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def hasdoublejump(i, game):
+        if not game.game_cars[i].double_jumped:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def quaterniontoeuler(q):
+        yaw = math.atan2(2*(q['x']*q['y'] + q['z']*q['w']), 1-2*(pow(q['y'], 2) + pow(q['z'], 2)))
+        pitch = math.asin(2*(q['x']*q['z']) - q['w']*q['y'])
+        roll = math.atan2(2*(q['x']*q['w'] + q['y']*q['z']), 1-2*(pow(q['z'], 2) + pow(q['w'], 2)))
+        return{
+            'x': yaw,
+            'y': pitch,
+            'z': roll
+        }
+
+    def closestcar(self, me, others, ball):
+        medist = self.distance(me['location'], ball['location'])
+        othersdist = [len(others)]
+        closest = -1
+        for i in range(len(others)):
+            othersdist[i] = self.distance(others[i]['location'], ball['location'])
+            if othersdist[i] < medist:
+                closest = i
+        return closest
+
+
+'''class Renderer(BaseAgent):
+
+    @staticmethod
+    def drawcurve(start, end, length, rotation):
+        # Draws a line from a to b of arc length c with rotation d radians when viewed from a looking at b.
+        # Done by using 10 small line segments connected by intermediate points
+        starttoend = UtilFunctions.distance(start, end)
+        if length >= starttoend:
+            dist = []
+        else:
+            pass
+'''
--- NeuralNetBots/HardCodeBot/states.py
+++ NeuralNetBots/HardCodeBot/states.py
@@ -1,41 +1,266 @@
-class StatesHandler:
+import math
+from NeuralNetBots.HardCodeBot.hardcodebot import *
 
-    def choosestate(self, ball, me, others):
+
+class StatesHandler:
+    def __init__(self):
         # Example list of state functions
-        self.statelist = {
-            'dribble': self.atba(ball,me,others),
-            'flick': "call()",
-            'pass': "call()",
-            '0': "call()",
-            '1': "call()",
-            '2': "call()",
-            '3': "call()",
-            '4': "call()",
-            '5': "call()",
-            '6': "call()",
-            '7': "call()",
-            '8': "call()",
-            '9': "call()"
+        self.state = {
+            'atba': {'active': False, 'call': ATBA.atba, 'finishcheck': ATBA.atbafinished},
+            'driveforward': {'active': False, 'call': DriveForward.driveforward,
+                             'finishcheck': DriveForward.driveforwardfinished},
+            '0': False,
+            '1': False,
+            '2': False,
+            '3': False
         }
 
-    def execute(self, state):
-        if '''state incomplete''':
-            self.stateList[state]
+    def choosestate(self, ball, me, others, info):
+        # Choose the state via conditions, wipe states, apply state
+        # Having a playtype selector might be useful (Shot, Defend, Aerial)
+        # If i'm not the fastest to the ball, determine offensive or defensive
+
+        # Temporary Test Code:
+        if self.state['atba']['active']:
+            nextstate = "driveforward"
         else:
-            return {
-                'Finished': True,
-                'throttle': 0,
-                'steer': 0,
-                'pitch': 0,
-                'yaw': 0,
-                'roll': 0,
-                'jump': 0,
-                'boost': 0,
-                'handbrake': 0
-            }
+            nextstate = "atba"
+        # End Test Code
+
+        self.statesclear()
+        self.state[nextstate]['call'] = True
+
+    def execute(self, state, ball, me, others, info):
+        self.state[state]['finished'](ball, me, others, info)
+        if self.state[state] and self.stateavailability[state](ball, me, others, info):
+            return self.statecalls[state](ball, me, others, info)
+        else:
+            self.choosestate(ball, me, others, info)
+
+    def carfieldposition(self, car):
+        '''
+        Definition of Positions on the field:
+        Looking from above, coordinate plane on field, (X team) goal at pos y and (Y team) goal at neg. y:
+        On ground: Defensive: 0, Offensive: 1
+        Lower Wall curves: Back: 2, Side Defensive: 3, Side Offensive: 4, Front: 5
+        Walls: Back Defensive: 6, Side Defensive: 7, Side Offensive: 8, Front Offensive: 9
+        Ceilings, Upper Corner & Wall Curves: Defensive: 10, Offensive: 11
+        Aerials: Defensive Low: 12, Defensive High: 13, Offensive Low: 14, Offensive High: 15
+        In-net: Defensive: 16, Offensive: 17
+        '''
+        # In net
+        if abs(car['location']['y']) >= 5120.0:
+            if self.ondefensive(me):
+                return 19
+            else:
+                return 20
+        # On the ground
+        elif car['wheelcontact'] and abs(car['rotation']['pitch']) <= math.pi/6.0 and \
+                abs(car['rotation']['yaw']) <= math.pi/6.0 and abs(car['rotation']['roll']) <= math.pi/6.0:
+            if self.ondefensive(car):
+                return 0
+            else:
+                return 1
+        # On the lower wall curves
+        elif car['wheelcontact'] and car['location']['z'] <= 40.0:
+            if self.ondefensive(car):
+                if self.backwall(car):
+                    return 2
+                else:
+                    return 3
+            else:
+                if self.backwall(car):
+                    return 4
+                else:
+                    return 5
+        # On the wall
+        elif car['wheelcontact'] and car['location']['z'] <= 2000.0:
+            if self.ondefensive(car):
+                if self.backwall(car):
+                    return 6
+                else:
+                    return 7
+            else:
+                if self.backwall(car):
+                    return 8
+                else:
+                    return 9
+        # Ceilings
+        elif car['wheelcontact']:
+            if self.ondefensive(car):
+                return 10
+            else:
+                return 11
+        # Aerial
+        else:
+            if self.ondefensive(car):
+                if car['location']['z'] <= 1000.0:
+                    return 12
+                else:
+                    return 13
+            else:
+                if car['location']['z'] <= 1000.0:
+                    return 14
+                else:
+                    return 15
+
+    def ballcondition(self, ball):
+        '''
+        Definition of ball conditions on the field:
+        Defensive Rolling on Ground: 0
+        Offensive Rolling on Ground: 1
+        Defensive Bouncing near ground: 2
+        Offensive Bouncing near ground: 3
+        Defensive Medium bouncing: 4
+        Offensive Medium bouncing: 5
+        Defensive High Bouncing: 6
+        Offensive High Bouncing: 7
+        Defensive On Side-wall: 8
+        Offensive On Side-wall: 9
+        On Back wall: 10
+        On Front wall: 11
+        General In-Air: 12
+        '''
+        # Check ball location and velocity:
+        # Side Walls
+        if abs(ball['location']['x']) > 3800.0 and abs(ball['velocity']['x']) < 150.0:
+            if self.ondefensive(ball['location']):
+                return 8
+            else:
+                return 9
+        # End Walls
+        elif abs(ball['location']['y']) > 4850.0 and abs(ball['velocity']['y']) < 150.0:
+            if self.ondefensive(ball['location']):
+                return 10
+            else:
+                return 11
+        # Not On A Wall
+        elif abs(ball['location']['x']) < 3800.0 and abs(ball['location']['y']) < 4850:
+            if ball['location']['z'] <= 100.0 and -16.7 <= ball['velocity']['z'] <= 10.0:
+                if self.ondefensive(ball['location']):
+                    return 0
+                else:
+                    return 1
+            elif ball['location']['z'] <= 300.0 and -167.0 <= ball['velocity']['z'] <= 100.0:
+                if self.ondefensive(ball['location']):
+                    return 2
+                else:
+                    return 3
+            elif ball['location']['z'] <= 600.0 and -670.0 <= ball['velocity']['z'] <= 400.0:
+                if self.ondefensive(ball['location']):
+                    return 4
+                else:
+                    return 5
+            else:
+                if self.ondefensive(ball['location']):
+                    return 6
+                else:
+                    return 7
+        else:
+            return 12
+
+    @staticmethod
+    def ondefensive(location):
+        if location['location']['y'] >= 0.0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def backwall(car):
+        if car['location']['y'] >= 5020.0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def controllerreturn(throttle=0.0, steer=0.0, pitch=0.0, yaw=0.0, roll=0.0, jump=False, boost=False, handbrake=False):
+        return {
+            'throttle': throttle,
+            'steer': steer,
+            'pitch': pitch,
+            'yaw': yaw,
+            'roll': roll,
+            'jump': jump,
+            'boost': boost,
+            'handbrake': handbrake
+        }
+
+    def statesclear(self):
+        for i in self.state:
+            self.state[i]['active'] = False
 
 
 class States:
 
-    def atba(self,ball,me,others):
-        pass
+    @staticmethod
+    def ground():
+        return StatesHandler.controllerreturn()
+
+    @staticmethod
+    def angle_2d(self, carvector, ballvector):
+        return math.atan(carvector['y'] / carvector['x']) - math.asin(
+            self.relativepos_2d(carvector, ballvector)['y'] / self.relativepos_2d(carvector, ballvector)['x'])
+
+    @staticmethod
+    def relativepos_2d(origin, location):
+        return {'x': location['x'] + origin['x'], 'y': location['y'] + location['y']}
+
+
+class ATBA(States):
+
+    carconditions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    ballconditions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    def atba(self, ball, me, others, info):
+        towardsball = self.angle_2d(me['rotation'], ball['location'])
+        steer = towardsball/math.pi/2
+        return StatesHandler.controllerreturn(False, 100, steer)
+
+    @staticmethod
+    def atbafinished(ball, me, others, info):
+        if info['player'] == HardCodeBot.myusername:
+            return True
+        else:
+            return False
+
+
+class DriveForward(States):
+
+    @staticmethod
+    def driveforward(me):
+        pitch = 0
+        yaw = 0
+        roll = 0
+        if me['rotation']['roll'] != 0:
+            roll = -1 * me['rotation']['roll']
+            if abs(roll) > 1:
+                roll = abs(roll)/roll
+
+        if me['rotation']['yaw'] != 0 or me['rotation']['yaw'] != math.pi:
+            if abs(me['rotation']['yaw'] - math.pi) <= abs(me['rotation']['yaw']):
+                yaw = -1 * (me['rotation']['yaw'] - math.pi)
+                if abs(yaw) > 1:
+                    yaw = abs(yaw)/yaw
+            else:
+                yaw = -1 * me['rotation']['yaw']
+                if abs(yaw) > 1:
+                    yaw = abs(yaw)/yaw
+        if me['rotationd']['pitch'] != 0:
+            pitch = -1 * me['rotation']['pitch']
+            if abs(pitch) > 1:
+                pitch = abs(pitch)/pitch
+        throttle = 1
+        boost = True
+        return StatesHandler.controllerreturn(throttle, 0, pitch, yaw, roll, False, boost, False)
+
+    @staticmethod
+    def driveforwardavailable():
+        return True
+
+    @staticmethod
+    def driveforwardfinished(ball, me, others, info):
+        if me['location']['y'] < 0:
+            return True
+        else:
+            return False
